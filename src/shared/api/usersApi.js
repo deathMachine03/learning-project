7124409bce0546da1dfa8249d28100e1236adc9d
deathMachine03 <s.abeshov@gmail.com>
2026-01-23 17:05:58 +0500

    service/getrequest

--- src/shared/api/usersApi.js
+++ src/shared/api/usersApi.js
@@ -1,12 +1,30 @@
-const BASE_URL = "https://jsonplaceholder.typicode.com"
+const BASE_URL = "https://jsonplaceholder.typicode.com";
 
-async function requestJson(path,options = {}) {
-    const res = await fetch(`${BASE_URL}{path}`, {
-        method: "GET",
-        headers: {
-            "Content-type": "application/json",
-            ...BASE_URL(options.headers || {}), 
-        },
-        ...options,
-    })
+async function requestJson(path, options = {}) {
+  const res = await fetch(`${BASE_URL}{path}`, {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json",
+      ...BASE_URL(options.headers || {}),
+    },
+    ...options,
+  });
+
+  if (!res.ok) {
+    const details = await res.text().catch(() => "");
+    const message = `HTTP {res.status} ${res.statusText}${details ? `: ${details}` : ""}`;
+    const err = new Error(message);
+    err.status = res.status;
+    throw err;
+  }
+
+  return res.json();
+}
+
+export async function getUsers() {
+  return requestJson("/users");
+}
+
+export async function getAdress(params) {
+    
 }
